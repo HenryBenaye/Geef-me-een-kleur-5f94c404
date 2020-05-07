fe58be06bb0b481d33c5e03e56ca5ed10e69a258
HenryBenaye <henry.be@outlook.com>
2020-05-07 13:10:37 +0200

    Uitwerking van de exercise

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function knop() {
-    document.querySelector("#box").innerText = "Verander de achtergrond kleur!";
-    document.querySelector("#box").style.backgroundColor = 'red';
-    // document.getElementById("box").style.backgroundColor = 'red';
+    var color = document.getElementById("colorChoice").value;
+    document.body.style.backgroundColor = color;
+    document.querySelector("body").innerText= "Verander achtergrond kleur!";
 }
